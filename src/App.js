118eb4daaf0d5fa1d2cbf6217482967b61bfa122
chaerulumam <chaerulumamb244@gmail.com>
2023-12-22 15:03:59 +0700

    feat: create file generate values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+    // function
+    const generateRandomString = (length) => {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        let result = ''
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length))
+        }
+
+        return result
+    }
+
+    // function
+    const generateRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    const [randomString, setRandomString] = useState('****')
+    const [randomNumber, setRandomNumber] = useState('****')
+
+    // function
+    const generateRandomValues = () => {
+        setRandomString(generateRandomString(3))
+        setRandomNumber(generateRandomNumber(10000, 99999))
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex items-center justify-center h-screen bg-cyan-950">
+      <div className="max-w-md mx-auto p-4 bg-gray-300 rounded-lg shadow-lg text-center">
+        <p className="text-2xl font-bold mb-4">{`SEQ${randomNumber}${randomString}`}</p>
+        <button
+          className="bg-slate-900 hover:bg-slate-950 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          onClick={generateRandomValues}
         >
-          Learn React
-        </a>
-      </header>
+          Create Reference
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
